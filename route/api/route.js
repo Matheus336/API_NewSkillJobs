b2b2955d47a2b182a6613206d8c8b9d9d700b244
Matheus <55720389+Matheus336@users.noreply.github.com>
2025-07-21 09:27:20 -0300

    Update route.js
    .

--- route/api/route.js
+++ route/api/route.js
@@ -10,60 +10,56 @@ const db = require('../../config/relational_db');
 
 const route = express.Router();
 /*
+**********************************************************************************************************************************************************************************************************
+*************************** NÃO USAR ESSE TRECHO DE CÓDIGO, ELE FORÇA A SINCRONIZAÇÃO DAS TABELAS NO BANCO DE DADOS, SE EXECUTADA EM PRODUÇÃO IRÁ DELETAR OS DADOS EXISTENTES. ***************************
+**********************************************************************************************************************************************************************************************************
+
 db.sequelize.sync({force: true})
-    .then(() => console.log("✅ Tabela 'usuário' sincronizada com o banco de dados"))
-    .catch(error => console.error("❌ Erro ao sincronizar tabela:", error))
+    .then(() => console.log("Tabelas sincronizada com o banco de dados"))
+    .catch(error => console.error("Erro ao sincronizar tabelas:", error))
 */
 module.exports = route;
 
-// Home
 route.get('/home', (req, res) => res.render('home'));
 
-// Rotas Applicant ok
-route.post('/applicant', controllerApplicant.postCreate); //
-route.get('/applicant', controllerApplicant.getList); //
-route.get('/applicant/:id', controllerApplicant.getApplicant); //
-route.put('/applicant/:id', controllerApplicant.putApplicant); //
-route.delete('/applicantDelete/:id', controllerApplicant.deleteApplicant); //
+route.post('/applicant', controllerApplicant.postCreate); 
+route.get('/applicant', controllerApplicant.getList); 
+route.get('/applicant/:id', controllerApplicant.getApplicant); 
+route.put('/applicant/:id', controllerApplicant.putApplicant); 
+route.delete('/applicantDelete/:id', controllerApplicant.deleteApplicant); 
 
-// Rotas Company ok
-route.post('/company', controllerCompany.postCreate); //
-route.get('/company', controllerCompany.getList); //
-route.get('/company/:id', controllerCompany.getCompany); //
-route.put('/company/:id', controllerCompany.putCompany); //
-route.delete('/companyDelete/:id', controllerCompany.deleteCompany); //
+route.post('/company', controllerCompany.postCreate); 
+route.get('/company', controllerCompany.getList); 
+route.get('/company/:id', controllerCompany.getCompany); 
+route.put('/company/:id', controllerCompany.putCompany); 
+route.delete('/companyDelete/:id', controllerCompany.deleteCompany); 
 
-// Rotas Contact ok
-route.get('/contact', controllerContacts.getList); //
-route.post('/contact', controllerContacts.postCreate); //
-route.get('/contact/:id', controllerContacts.getContact); //
-route.put('/contact/:id', controllerContacts.putContact); //
-route.delete('/contact/:id', controllerContacts.deleteContact); //
+route.get('/contact', controllerContacts.getList); 
+route.post('/contact', controllerContacts.postCreate); 
+route.get('/contact/:id', controllerContacts.getContact); 
+route.put('/contact/:id', controllerContacts.putContact); 
+route.delete('/contact/:id', controllerContacts.deleteContact); 
 
-// Rotas Job_Application ok
-route.get('/application', controllerJobApplication.getList); //
-route.post('/application', controllerJobApplication.postCreate); //
-route.get('/application/:id', controllerJobApplication.getJobApplication); //
-route.put('/application/:id', controllerJobApplication.putJobApplication); //
-route.delete('/application/:id', controllerJobApplication.deleteJobApplication); //
+route.get('/application', controllerJobApplication.getList); 
+route.post('/application', controllerJobApplication.postCreate); 
+route.get('/application/:id', controllerJobApplication.getJobApplication); 
+route.put('/application/:id', controllerJobApplication.putJobApplication); 
+route.delete('/application/:id', controllerJobApplication.deleteJobApplication); 
 
-// Rotas para Job Vacancy ok
-route.get('/jobVacancy', controllerJobVacancy.getList); //
-route.post('/jobVacancy', controllerJobVacancy.postCreate); //
-route.get('/jobVacancy/:id', controllerJobVacancy.getJobVacancy); //
-route.put('/jobVacancy/:id', controllerJobVacancy.putJobVacancy); //
-route.delete('/jobVacancy/:id', controllerJobVacancy.deleteJobVacancy); //
+route.get('/jobVacancy', controllerJobVacancy.getList); 
+route.post('/jobVacancy', controllerJobVacancy.postCreate); 
+route.get('/jobVacancy/:id', controllerJobVacancy.getJobVacancy); 
+route.put('/jobVacancy/:id', controllerJobVacancy.putJobVacancy); 
+route.delete('/jobVacancy/:id', controllerJobVacancy.deleteJobVacancy); 
 
-// Controller Curriculum ok
-route.get('/curriculum', controllerCurriculum.getList); //
-route.get('/curriculum/:id', controllerCurriculum.getCurriculum); //
-route.post('/curriculum', controllerCurriculum.postCreate); //
-route.put('/curriculum/:id', controllerCurriculum.putCurriculum); //
-route.delete('/curriculum/:id', controllerCurriculum.deleteCurriculum); //
+route.get('/curriculum', controllerCurriculum.getList); 
+route.get('/curriculum/:id', controllerCurriculum.getCurriculum); 
+route.post('/curriculum', controllerCurriculum.postCreate); 
+route.put('/curriculum/:id', controllerCurriculum.putCurriculum); 
+route.delete('/curriculum/:id', controllerCurriculum.deleteCurriculum); 
 
-// Controller Feedback ok
-route.get('/feedback', controllerFeedback.getList); //
-route.get('/feedback/:id', controllerFeedback.getFeedback); //
-route.post('/feedback', controllerFeedback.postCreate); //
-route.put('/feedback/:id', controllerFeedback.putFeedback); //
-route.delete('/feedback/:id', controllerFeedback.deleteFeedback); //
+route.get('/feedback', controllerFeedback.getList); 
+route.get('/feedback/:id', controllerFeedback.getFeedback); 
+route.post('/feedback', controllerFeedback.postCreate); 
+route.put('/feedback/:id', controllerFeedback.putFeedback); 
+route.delete('/feedback/:id', controllerFeedback.deleteFeedback); 
